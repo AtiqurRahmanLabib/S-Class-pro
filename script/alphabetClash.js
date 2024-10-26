6a87ec75a668db5c2398d5d9a0fc6bb5ba75b857
AtiqurRahmanLabib <atiqurrahmanlabib@gmail.com>
2024-10-26 13:08:18 +0600

    added display

---
+++ script/alphabetClash.js
@@ -0,0 +1,12 @@
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    setBackgroundColorById(alphabet);
+}
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
